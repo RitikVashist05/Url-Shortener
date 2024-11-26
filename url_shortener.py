0615778d0cae6a13c4cbe8a6d977ceac9be55b43
RitikVashist05 <ritikvashist05@gmail.com>
2024-11-26 22:04:34 +0530

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -7,8 +7,11 @@ class URLShortener:
 
     def shorten_url(self, original_url):
         short_code = generate_short_code()
+        while self.db.get_url(short_code):  
+            short_code = generate_short_code()
         self.db.store_url(short_code, original_url)
         return short_code
 
     def expand_url(self, short_code):
         return self.db.get_url(short_code)
+
