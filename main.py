27b16af0cc8fc8c7aa94c786c8fcec8cd3d23c15
RitikVashist05 <ritikvashist05@gmail.com>
2024-11-26 22:03:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, jsonify
 from url_shortener import URLShortener
+import validators
 
 app = Flask(__name__)
 shortener = URLShortener()
@@ -9,6 +10,8 @@ def shorten_url():
     original_url = request.json.get('url')
     if not original_url:
         return jsonify({'error': 'URL is required'}), 400
+    if not validators.url(original_url):
+        return jsonify({'error': 'Invalid URL'}), 400
 
     short_url = shortener.shorten_url(original_url)
     return jsonify({'short_url': request.host_url + short_url}), 201
@@ -22,3 +25,4 @@ def redirect_url(short_code):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
